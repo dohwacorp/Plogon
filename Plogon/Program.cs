b54c994d84e18cd79f1ae0e818892de3dfb2436c
DoHwa <kangdohwa0310@gmail.com>
2023-05-08 12:36:32 +0900

    no idea

--- Plogon/Program.cs
+++ Plogon/Program.cs
@@ -31,7 +31,7 @@ class Program
         SetupLogging();
 
         var webhook = new DiscordWebhook();
-        //var webservices = new WebServices();
+        var webservices = new WebServices();
         
         var githubSummary = "## Build Summary\n";
         GitHubOutputBuilder.SetActive(ci);
@@ -231,38 +231,38 @@ class Program
                                     $"v{status.Version} - [Diff]({status.DiffUrl})");
                             }
 
-                            //if (!string.IsNullOrEmpty(prNumber) && !commit)
-                            //    await webservices.RegisterPrNumber(task.InternalName, task.Manifest.Plugin.Commit,
-                            //        prNumber);
+                            if (!string.IsNullOrEmpty(prNumber) && !commit)
+                                await webservices.RegisterPrNumber(task.InternalName, task.Manifest.Plugin.Commit,
+                                    prNumber);
 
                             if (commit)
                             {
                                 int? prInt = null;
-                                //if (int.TryParse(
-                                //        await webservices.GetPrNumber(task.InternalName, task.Manifest.Plugin.Commit),
-                                //        out var commitPrNum))
-                                //{
-                                //    // Let's try again here in case we didn't get it the first time around
-                                //    if (string.IsNullOrEmpty(changelog) && repoName != null &&
-                                //        gitHubApi != null)
-                                //    {
-                                //        changelog = await gitHubApi.GetIssueBody(commitPrNum);
-                                //    }
+                                if (int.TryParse(
+                                        await webservices.GetPrNumber(task.InternalName, task.Manifest.Plugin.Commit),
+                                        out var commitPrNum))
+                                {
+                                    // Let's try again here in case we didn't get it the first time around
+                                    if (string.IsNullOrEmpty(changelog) && repoName != null &&
+                                        gitHubApi != null)
+                                    {
+                                        changelog = await gitHubApi.GetIssueBody(commitPrNum);
+                                    }
 
-                                //    prInt = commitPrNum;
-                                //}
+                                    prInt = commitPrNum;
+                                }
 
-                                //await webservices.StagePluginBuild(new WebServices.StagedPluginInfo
-                                //{
-                                //    InternalName = task.InternalName,
-                                //    Version = status.Version!,
-                                //    Dip17Track = task.Channel,
-                                //    PrNumber = prInt,
-                                //    Changelog = changelog,
-                                //    IsInitialRelease = task.IsNewPlugin,
-                                //    DiffLinesAdded = status.DiffLinesAdded,
-                                //    DiffLinesRemoved = status.DiffLinesRemoved,
-                                //});
+                                await webservices.StagePluginBuild(new WebServices.StagedPluginInfo
+                                {
+                                    InternalName = task.InternalName,
+                                    Version = status.Version!,
+                                    Dip17Track = task.Channel,
+                                    PrNumber = prInt,
+                                    Changelog = changelog,
+                                    IsInitialRelease = task.IsNewPlugin,
+                                    DiffLinesAdded = status.DiffLinesAdded,
+                                    DiffLinesRemoved = status.DiffLinesRemoved,
+                                });
 
                                 if (status.DiffLinesAdded.HasValue)
                                 {
@@ -347,8 +347,8 @@ class Program
 
                 if (repoName != null && prNumber != null)
                 {
-                    //var existingMessages = await webservices.GetMessageIds(prNumber);
-                    //var alreadyPosted = existingMessages.Length > 0;
+                    var existingMessages = await webservices.GetMessageIds(prNumber);
+                    var alreadyPosted = existingMessages.Length > 0;
 
                     var links =
                         $"[Show log](https://github.com/dohwacorp/DalamudPluginsD17/actions/runs/{actionRunId}) - [Review](https://github.com/goatcorp/DalamudPluginsD17/pull/{prNumber}/files#submit-review)";
@@ -374,20 +374,18 @@ class Program
                     var hookTitle = $"PR #{prNumber}";
                     var buildInfo = string.Empty;
 
-                    //if (!alreadyPosted)
-                    //{
-                    //    hookTitle += " created";
+                    if (!alreadyPosted)
+                    {
+                        hookTitle += " created";
 
-                    //    var prDesc = await gitHubApi!.GetIssueBody(prNum);
-                    //    if (!string.IsNullOrEmpty(prDesc))
-                    //        buildInfo += $"```\n{prDesc}\n```\n";
-                    //}
-                    //else
-                    //{
-                    //    hookTitle += " updated";
-                    //}
-
-                    hookTitle += " updated";
+                        var prDesc = await gitHubApi!.GetIssueBody(prNum);
+                        if (!string.IsNullOrEmpty(prDesc))
+                            buildInfo += $"```\n{prDesc}\n```\n";
+                    }
+                    else
+                    {
+                        hookTitle += " updated";
+                    }
 
                     buildInfo += anyTried ? buildsMd.GetText(true) : "No builds made.";
                     buildInfo = ReplaceDiscordEmotes(buildInfo);
@@ -403,10 +401,10 @@ class Program
                     var id = await webhook.Send(ok ? Color.Purple : Color.Red,
                         $"{buildInfo}\n\n{links} - [PR](https://github.com/dohwacorp/DalamudPluginsD17/pull/{prNumber})",
                         hookTitle, ok ? "Accepted" : "Rejected");
-                    //await webservices.RegisterMessageId(prNumber!, id);
+                    await webservices.RegisterMessageId(prNumber!, id);
 
-                    //if (gitHubApi != null)
-                    //    await gitHubApi.SetPrLabels(prNum, prLabels);
+                    if (gitHubApi != null)
+                        await gitHubApi.SetPrLabels(prNum, prLabels);
                 }
 
                 if (repoName != null && commit && anyTried)
@@ -416,63 +414,63 @@ class Program
                         "Builds committed", string.Empty);
 
                     // TODO: We don't support this for removals for now
-                    //foreach (var buildResult in statuses.Where(x => x.Task.Type == BuildTask.TaskType.Build))
-                    //{
-                    //    if (!buildResult.Success && !aborted)
-                    //        continue;
+                    foreach (var buildResult in statuses.Where(x => x.Task.Type == BuildTask.TaskType.Build))
+                    {
+                        if (!buildResult.Success && !aborted)
+                            continue;
 
-                    //    var resultPrNum =
-                    //        await webservices.GetPrNumber(buildResult.Task.InternalName, buildResult.Task.Manifest!.Plugin.Commit);
-                    //    if (resultPrNum == null)
-                    //    {
-                    //        Log.Warning("No PR for {InternalName} - {Version}", buildResult.Task.InternalName,
-                    //            buildResult.Version);
-                    //        continue;
-                    //    }
+                        var resultPrNum =
+                            await webservices.GetPrNumber(buildResult.Task.InternalName, buildResult.Task.Manifest!.Plugin.Commit);
+                        if (resultPrNum == null)
+                        {
+                            Log.Warning("No PR for {InternalName} - {Version}", buildResult.Task.InternalName,
+                                buildResult.Version);
+                            continue;
+                        }
 
-                    //    try
-                    //    {
-                    //        var msgIds = await webservices.GetMessageIds(resultPrNum);
+                        try
+                        {
+                            var msgIds = await webservices.GetMessageIds(resultPrNum);
 
-                    //        foreach (var id in msgIds)
-                    //        {
-                    //            await webhook.Client.ModifyMessageAsync(ulong.Parse(id), properties =>
-                    //            {
-                    //                var embed = properties.Embeds.Value.First();
-                    //                var newEmbed = new EmbedBuilder()
-                    //                    .WithColor(Color.LightGrey)
-                    //                    .WithTitle(embed.Title)
-                    //                    .WithCurrentTimestamp()
-                    //                    .WithDescription(embed.Description);
+                            foreach (var id in msgIds)
+                            {
+                                await webhook.Client.ModifyMessageAsync(ulong.Parse(id), properties =>
+                                {
+                                    var embed = properties.Embeds.Value.First();
+                                    var newEmbed = new EmbedBuilder()
+                                        .WithColor(Color.LightGrey)
+                                        .WithTitle(embed.Title)
+                                        .WithCurrentTimestamp()
+                                        .WithDescription(embed.Description);
 
-                    //                if (embed.Author.HasValue)
-                    //                    newEmbed = newEmbed.WithAuthor(embed.Author.Value.Name,
-                    //                        embed.Author.Value.IconUrl,
-                    //                        embed.Author.Value.Url);
+                                    if (embed.Author.HasValue)
+                                        newEmbed = newEmbed.WithAuthor(embed.Author.Value.Name,
+                                            embed.Author.Value.IconUrl,
+                                            embed.Author.Value.Url);
 
-                    //                if (embed.Footer.HasValue)
-                    //                {
-                    //                    if (embed.Footer.Value.Text.Contains("Comment"))
-                    //                    {
-                    //                        newEmbed = newEmbed.WithFooter(
-                    //                            embed.Footer.Value.Text.Replace("Comment", "Committed"),
-                    //                            embed.Footer.Value.IconUrl);
-                    //                    }
-                    //                    else
-                    //                    {
-                    //                        newEmbed = newEmbed.WithFooter("Committed");
-                    //                    }
-                    //                }
+                                    if (embed.Footer.HasValue)
+                                    {
+                                        if (embed.Footer.Value.Text.Contains("Comment"))
+                                        {
+                                            newEmbed = newEmbed.WithFooter(
+                                                embed.Footer.Value.Text.Replace("Comment", "Committed"),
+                                                embed.Footer.Value.IconUrl);
+                                        }
+                                        else
+                                        {
+                                            newEmbed = newEmbed.WithFooter("Committed");
+                                        }
+                                    }
 
-                    //                properties.Embeds = new[] { newEmbed.Build() };
-                    //            });
-                    //        }
-                    //    }
-                    //    catch (Exception ex)
-                    //    {
-                    //        Log.Error(ex, "Could not update messages");
-                    //    }
-                    //}
+                                    properties.Embeds = new[] { newEmbed.Build() };
+                                });
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Error(ex, "Could not update messages");
+                        }
+                    }
                 }
             }
         }
